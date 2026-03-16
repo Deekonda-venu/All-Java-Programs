ef757cbfe1f2e4465f283a81d09f559aaa88bb38
Deekonda_venu <venudeekonda786@gmail.com>
2026-03-16 23:54:41 +0530

    Sorting

--- src/Sorting/Selectionsorting.java
+++ src/Sorting/Selectionsorting.java
@@ -6,7 +6,7 @@ public class Selectionsorting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int len = sc.nextInt();
-        int[] arrey = new int[];
+        int[] arrey = new int[len];
         for(int i=0; i<len; i++){
             arrey[i] = sc.nextInt();
         }
@@ -15,9 +15,22 @@ public class Selectionsorting {
         }
         System.out.println();
         for(int i=0; i<len; i++){
+            int temp =arrey[i];
+            int index = 0;
+            for (int j=i; j<len; j++){
+                if(arrey[j]>temp){
+                    temp = arrey[j];
+                    index = j;
+                }
+            }
+            arrey[index] = arrey[i];
+            arrey[i] = temp;
+
 
         }
-
+        for(int i=0; i<len; i++){
+            System.out.print(arrey[i] + " ");
+        }
 
     }
 }
