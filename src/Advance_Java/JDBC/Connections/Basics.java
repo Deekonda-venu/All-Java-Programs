8d7e543f70590fa96e2fdbf0e562c60e9308041d
Deekonda_venu <venudeekonda786@gmail.com>
2026-04-20 10:33:44 +0530

    Merge remote-tracking branch 'origin/venu-2Darey' into venu-2Darey

---
+++ src/Advance_Java/JDBC/Connections/Basics.java
@@ -0,0 +1,56 @@
+package Advance_Java.JDBC.Connections;
+
+//What is JDBC?
+//
+//        👉 JDBC = Java Database Connectivity
+//
+//👉 It is used to:
+//
+//Connect Java program to database
+//Execute SQL queries
+//Get results
+
+//Java Program → JDBC → Database (MySQL)Database
+
+//JDBC Architecture (Basic Idea)
+//
+//👉 JDBC acts like a bridge between:
+//
+//Java application
+//Database
+
+//Core comectps
+
+//1. Driver
+//
+//👉 Connects Java to database
+//
+//Example:
+////
+//com.mysql.cj.jdbc.Driver
+//✅ 2. Connection
+//
+//👉 Represents connection to DB
+//
+//Connection con;
+//✅ 3. Statement
+//
+//👉 Used to execute SQL
+//
+//Statement stmt;
+//✅ 4. ResultSet
+//
+//👉 Stores data from SELECT query
+//
+//ResultSet rs;
+//✅ 5. DriverManager
+//
+//👉 Helps to create connection
+//
+//DriverManager.getConnection()
+public class Basics {
+
+
+
+
+}
