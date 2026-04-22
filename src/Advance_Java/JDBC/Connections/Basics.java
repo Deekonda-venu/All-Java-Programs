e6cbc2ff83a3a8efdd02d850330cc959b13b2fc0
Deekonda_venu <venudeekonda786@gmail.com>
2026-04-22 22:52:15 +0530

    Merge remote-tracking branch 'origin/venu-2Darey' into venu-2Darey

--- src/Advance_Java/JDBC/Connections/Basics.java
+++ src/Advance_Java/JDBC/Connections/Basics.java
@@ -26,6 +26,12 @@ package Advance_Java.JDBC.Connections;
 //👉 Connects Java to database
 //
 //Example:
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 ////
 //com.mysql.cj.jdbc.Driver
 //✅ 2. Connection
@@ -49,6 +55,34 @@ package Advance_Java.JDBC.Connections;
 //
 //DriverManager.getConnection()
 public class Basics {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "root@123");
+
+            System.out.println("Connection established");
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM user");
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+
+                System.out.println(id + " " + name);
+            }
+
+            // 6. Close Connection
+            con.close();
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 
